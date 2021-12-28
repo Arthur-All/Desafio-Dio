ab529c0671991edf20a9b32f7e67ec54bf435ca7
Arthur.R.A <82613934+Arthur-All@users.noreply.github.com>
2021-12-28 05:55:02 -0800

    csharp com o prof edu

---
+++ Alunas com Eduador Lima/POO/ExemploPOO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ExemploPOO;
+using ExemploPOO.Interfaces;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        ICalculadora calc = new Calculadora();
+
+            
+            
+            // Calculadora calc = new Calculadora();
+            // System.Console.WriteLine("Resultado da primeira soma: " + calc.Somar(10, 10));
+            // System.Console.WriteLine("Resultado da segunda soma: " + calc.Somar(10, 10, 10));
+            
+            // Aluno p1 = new Aluno();
+            // p1.Nota = 10;
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+
+            // p1.Apresentar();
+
+            // Professor p2 = new Professor();
+            // p2.Salario = 3500;
+            // p2.Nome = "Michael";
+            // p2.Idade = 30;
+
+            // p2.Apresentar();
+            
+            
+            
+            // // Valores validos
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(30, 30);
+            // System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            // // Valores invalidos
+            // Retangulo r2 = new Retangulo();
+            // r2.DefinirMedidas(0, 0);
+            // System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            // Pessoa p1 = new Pessoa();
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+
+            // p1.Apresentar();
+        }    
+}   
---
+++ Alunas com Eduador Lima/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+    int [] [] VetorBidimensal = new int [] [] {
+        new int [] {1,2},
+        new int [] {1,2,3},
+        new int [] {1,2,3,4}
+    };
+
+    foreach(int [] array in VetorBidimensal)
+    {
+        foreach(int e in array)
+        {
+            Console.WriteLine(e + ' ');
+        }
+    }
+
+
